e557a79ca1f0c4d462a1d36f1af58bd6b2a4f5de
vaknin <vaknins33@gmail.com>
2019-07-24 22:18:44 +0300

    initial commit

---
+++ scripts/background.js
@@ -0,0 +1,10 @@
+chrome.commands.onCommand.addListener(commands => {
+
+    //Toggle Edit Mode Keyboard Shortcut
+    if (command == 'toggle-edit-mode'){
+
+    }
+ });
+
+ console.log('start');
+ 
---
+++ scripts/content.js
@@ -0,0 +1,6 @@
+// Change the active tab's title
+function setName(name){
+    document.title = name;
+}
+
+//setName('lel');
